31df5e05a21c295fd6f5037d4032bd263b59e376
EKR <ekr@rtfm.com>
2016-12-15 07:41:00 -0800

    Read in test cases

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+extern crate rustc_serialize;
 extern crate mio;
 use mio::*;
 use mio::tcp::{TcpListener, TcpStream};
-use std::io::Read;
-use std::io::Write;
+use rustc_serialize::json;
+use std::env;
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
 use std::process::{Child, Command, ExitStatus};
 
 const CLIENT: Token = mio::Token(0);
@@ -19,7 +23,7 @@ struct Agent {
 }
 
 impl Agent {
-    fn new(name: String, path: String, args: Vec<String>) -> Result<Agent, i32> {
+    fn new(name: &String, path: &String, args: Vec<String>) -> Result<Agent, i32> {
         let addr = "127.0.0.1:13265".parse().unwrap();
         let listener = TcpListener::bind(&addr).unwrap();
         
@@ -51,8 +55,8 @@ impl Agent {
                     println!("Accepted");
 
                     return Ok(Agent {
-                        name: name,
-                        path: path,
+                        name: name.clone(),
+                        path: path.clone(),
                         args: args,
                         socket: sock.unwrap().0,
                         child: child,
@@ -136,25 +140,59 @@ fn shuttle(client: &mut Agent, server: &mut Agent) {
     }
 }
 
-fn main() {
-    let client_shim = String::from("/Users/ekr/dev/nss-dev/nss-sandbox2/dist/Darwin15.6.0_cc_64_DBG.OBJ/bin/nss_bogo_shim");
-    let server_shim = String::from("/Users/ekr/dev/nss-dev/nss-sandbox2/dist/Darwin15.6.0_cc_64_DBG.OBJ/bin/nss_bogo_shim");
-//    let client_shim = String::from("/Users/ekr/dev/boringssl/build/ssl/test/bssl_shim");
-//    let server_shim = String::from("/Users/ekr/dev/boringssl/build/ssl/test/bssl_shim");
+#[derive(RustcDecodable, RustcEncodable)]
+#[derive(Debug)]
+struct TestCaseAgent {
+    flags : Option<Vec<String>>,
+}
 
-    let mut server = Agent::new(String::from("server"),
-                                server_shim.clone(),
+#[derive(RustcDecodable, RustcEncodable)]
+#[derive(Debug)]
+struct TestCase {
+    name: String,
+    versions : Option<Vec<String>>,
+    client : Option<TestCaseAgent>,
+    server : Option<TestCaseAgent>,
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
+#[derive(Debug)]
+struct TestCases {
+    cases : Vec<TestCase>,
+}
+
+struct TestConfig {
+    client_shim : String,
+    server_shim : String,
+}
+
+fn run_test_case(config: &TestConfig, case: &TestCase) {
+    let mut server = Agent::new(&String::from("server"),
+                                &config.server_shim,
                                 vec![
                                     String::from("-server"),
                                     String::from("-key-file"),
                                     String::from("/Users/ekr/dev/boringssl/ssl/test/runner/rsa_1024_key.pem"),
                                     String::from("-cert-file"),
                                     String::from("/Users/ekr/dev/boringssl/ssl/test/runner/rsa_1024_cert.pem")]).unwrap();
-    let mut client = Agent::new(String::from("client"),
-                                client_shim.clone(),
+    let mut client = Agent::new(&String::from("client"),
+                                &config.client_shim,
                                 vec![]).unwrap();
     shuttle(&mut client, &mut server);
 
     client.check_status();
     server.check_status();
+}
+
+
+fn main() {
+    let config = TestConfig {
+        client_shim : String::from("/Users/ekr/dev/nss-dev/nss-sandbox2/dist/Darwin15.6.0_cc_64_DBG.OBJ/bin/nss_bogo_shim"),
+        server_shim : String::from("/Users/ekr/dev/nss-dev/nss-sandbox2/dist/Darwin15.6.0_cc_64_DBG.OBJ/bin/nss_bogo_shim"),
+    };
+
+    let mut f = File::open("cases.json").unwrap();
+    let mut s = String::from("");
+    f.read_to_string(&mut s).expect("Could not read file to string");
+    let t : TestCases = json::decode(&s).unwrap();
 }
